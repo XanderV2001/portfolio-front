745448c0c8cacfc119063c76125a25ae9373fa4c
Xander Vos <2001xvos@gmail.com>
2021-02-24 18:18:17 +0100

    Pre redering squad roll out :)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
+var path = require('path')
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  configureWebpack: config => {
+    if (process.env.NODE_ENV !== 'production') return
+ 
+    return {
+      plugins: [
+        new PrerenderSpaPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, 'dist'),
+          // List of routes to prerender
+          [ '/', '/about'],
+        ),
+      ]
+    }
+  }
 }
