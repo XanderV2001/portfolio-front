4cf7028d3470fe31a0dd8edcc1fdf300677d06c4
Xander Vos <2001xvos@gmail.com>
2021-06-29 00:05:50 +0200

    added projects

--- src/components/home/projects.vue
+++ src/components/home/projects.vue
@@ -6,8 +6,12 @@
       :key="project.id"
     >
       <v-sheet color="secondary" height="100%">
-        <v-row class="fill-height" align="center" justify="center">
-          <div class="display-3">{{ project.title }}</div>
+        <v-row class="fill-height flex-column" align="center" justify="center">
+          <div class="display-3 mb-10">{{ project.title }}</div>
+          <p class="mb-15 text-h6">{{ project.description }}</p>
+          <v-btn :href="project.url" target="_blank" outlined>
+            More information
+          </v-btn>
         </v-row>
       </v-sheet>
     </v-carousel-item>
@@ -21,11 +25,15 @@ export default {
     projects: [
       {
         id: "1",
-        title: "Project 1",
+        title: "Voltiac Development",
+        description: "Co-founded venture for software development",
+        url: "https://voltiac.dev/",
       },
       {
         id: "2",
-        title: "Project 2",
+        title: "PC Parts API",
+        description: "Open-Source PC Parts API built with GraphQL",
+        url: "https://parts.xandervos.nl/",
       },
     ],
   }),
