6fabb391f8148223341bb28194abc8e13f7cf8c1
Xander Vos <2001xvos@gmail.com>
2022-10-07 00:25:46 +0200

    responsive design

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -2,9 +2,9 @@
 
     <div class="w-screen h-screen-navbar flex justify-center items-center flex-col">
 
-        <h1 class="text-8xl mb-10">Xander Vos Development</h1>
+        <h1 class="md:text-8xl text-5xl mb-10 text-center">Xander Vos Development</h1>
 
-        <div class="flex flex-row gap-20">
+        <div class="flex flex-row gap-10 md:gap-20">
             <n-button text type="primary" class="z-10" size="large" href="https://www.linkedin.com/in/xander-vos/"
                 tag="a" target="_blank">
                 <n-icon size="35">
