cec1596d8a146662b9a60bb74daaf9cc85273727
Xander Vos <xanderv2001@proton.me>
2023-10-29 15:26:02 +0100

    Reactive optimizations

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,11 +1,12 @@
 <template>
     <section class="container mx-auto px-4 flex gap-10 flex-col py-2">
-        <div class="w-full flex gap-16 py-20 px-10">
+        <div class="w-full flex gap-6 md:gap-16 pt-20 pb-12 px-10 flex-col sm:flex-row items-center">
             <img src="/xander.jpg" class="rounded drop-shadow-lg w-48 sm:w-52 md:w-60 lg:w-80" />
-            <div class="flex flex-col gap-5">
+            <div class="flex flex-col justify-around">
                 <p class="xl:text-9xl lg:text-7xl md:text-5xl sm:text-4xl text-4xl tracking-wider font-bold"> Xander
                     Vos </p>
-                <p class="xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-2xl italic">Full-stack Software Engineer</p>
+                <p class="xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-2xl italic py-6 md:py-12">Full-stack Software
+                    Engineer</p>
                 <div class="flex justify-around">
                     <a :href=link.url target="_blank"
                         class="w-10 flex justify-center items-center hover:scale-110 hover:text-green-500 transition "
@@ -17,68 +18,43 @@
         </div>
         <p class="text-lg">
             Hello! I'm a passionate and versatile full stack software engineer with a strong commitment to crafting
-            robust,
-            efficient, and innovative solutions. My journey in the world of software development began with a deep
-            curiosity
-            and
-            a desire to create meaningful and user-friendly applications. Since then, I've dedicated myself to
-            honing my
-            skills
-            and staying at the forefront of technology to deliver exceptional results.
+            robust, efficient, and innovative solutions. My journey in the world of software development began with a deep
+            curiosity and a desire to create meaningful and user-friendly applications. Since then, I've dedicated myself to
+            honing my skills and staying at the forefront of technology to deliver exceptional results.
         </p>
         <p class="text-lg">
             My love for software engineering stems from the thrill of turning ideas into reality. I thrive on
             problem-solving,
             taking on challenges that require a mix of creativity and technical expertise. Whether it's developing
-            responsive
-            web applications, building scalable back-end systems, or optimizing databases, I relish the opportunity
-            to
-            make
-            a
-            positive impact through code.
+            responsive web applications, building scalable back-end systems, or optimizing databases, I relish the
+            opportunity
+            to make a positive impact through code.
         </p>
         <p class="text-lg">
             My technical toolkit includes a wide range of languages, frameworks, and tools, enabling me to tackle
-            both
-            front-end
-            and back-end development with ease. I've designed and developed user-friendly interfaces that not only
-            look
-            great
-            but also provide a seamless user experience. My expertise in crafting efficient server-side solutions
-            ensures
-            that
-            applications run smoothly, even under high loads.
+            both front-end and back-end development with ease. I've designed and developed user-friendly interfaces that not
+            only
+            look great but also provide a seamless user experience. My expertise in crafting efficient server-side solutions
+            ensures that applications run smoothly, even under high loads.
         </p>
         <p class="text-lg">
             I'm a strong advocate for best practices in software development, including test-driven development,
-            version
-            control, and continuous integration. Collaboration and communication are also vital aspects of my work;
-            I
-            thrive
-            in
-            a team environment and am skilled at translating complex technical concepts into plain language for
+            version control, and continuous integration. Collaboration and communication are also vital aspects of my work;
+            I thrive in a team environment and am skilled at translating complex technical concepts into plain language for
             stakeholders.
         </p>
         <p class="text-lg">
             With a passion for lifelong learning, I stay up-to-date with the latest industry trends, ensuring my
-            skills
-            remain
-            current and adaptable to emerging technologies. I believe that there is always room for improvement and
-            innovation,
-            and I embrace the opportunity to learn and grow in this ever-evolving field.
+            skills remain current and adaptable to emerging technologies. I believe that there is always room for
+            improvement and
+            innovation, and I embrace the opportunity to learn and grow in this ever-evolving field.
         </p>
         <p class="text-lg">
             I'm excited about the endless possibilities in the software engineering realm, and I'm always looking
-            for
-            new
-            challenges and opportunities to contribute to projects that make a difference. If you're looking for a
-            dedicated,
-            creative, and experienced full stack software engineer to join your team or collaborate on your next
-            project,
-            I'd
-            love to connect and explore how we can achieve greatness together. Let's bring your ideas to life
-            through
-            code!
+            for new challenges and opportunities to contribute to projects that make a difference. If you're looking for a
+            dedicated, creative, and experienced full stack software engineer to join your team or collaborate on your next
+            project, I'd love to connect and explore how we can achieve greatness together. Let's bring your ideas to life
+            through code!
         </p>
 
     </section>
